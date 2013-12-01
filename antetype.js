e33b57bb5f6ddda47ce012f0598de317b52e4404
Florian Schulz <mail@getflourish.com>
2013-12-01 18:59:36 +0100

    Draggable

--- antetype.js
+++ antetype.js
@@ -40,7 +40,7 @@ function LayerController($scope) {
   });
 }
 
-angular.module('antetypeApp', ['myDirectives']);
+angular.module('antetypeApp', ['myDirectives', 'draggableModule']);
 
 
 angular.module('myDirectives', []).
@@ -76,3 +76,40 @@ angular.module('myDirectives', []).
           }
       };
   });
+
+angular.module('draggableModule', []).
+directive('draggable', ['$document' , function($document) {
+  return {
+    restrict: 'A',
+    link: function(scope, elm, attrs) {
+      var startX, startY, initialMouseX, initialMouseY;
+      elm.css({position: 'absolute'});
+      elm.addClass("ui-draggable");
+
+      elm.bind('mousedown', function($event) {
+        startX = elm.prop('offsetLeft');
+        startY = elm.prop('offsetTop');
+        initialMouseX = $event.clientX;
+        initialMouseY = $event.clientY;
+        $document.bind('mousemove', mousemove);
+        $document.bind('mouseup', mouseup);
+        return false;
+      });
+
+      function mousemove($event) {
+        var dx = $event.clientX - initialMouseX;
+        var dy = $event.clientY - initialMouseY;
+        elm.css({
+          top:  startY + dy + 'px',
+          left: startX + dx + 'px'
+        });
+        return false;
+      }
+
+      function mouseup() {
+        $document.unbind('mousemove', mousemove);
+        $document.unbind('mouseup', mouseup);
+      }
+    }
+  };
+}]);
